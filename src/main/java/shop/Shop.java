4ddc465c428aae17761c9aa8c5fba6caec93ab1b
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-11 17:27:31 +0100

    shop and shop test set up, count of stock in shop starts at 0

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,17 @@
 package shop;
 
+import interfaces.Sellable;
+
+import java.util.ArrayList;
+
 public class Shop {
+    private ArrayList<Sellable> stock;
+
+    public Shop() {
+        this.stock = new ArrayList<>();
+    }
+
+    public int stockCount() {
+        return stock.size();
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import shop.Shop;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+
+    @Before
+    public void before() {
+        shop = new Shop();
+
+    }
+
+    @Test
+    public void itemCountStartsAt0(){
+        assertEquals(0, shop.stockCount());
+    }
 }
