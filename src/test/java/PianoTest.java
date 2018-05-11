7877656561e7d9ea3570de5ea0ed43a8f13b1e20
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-11 12:20:04 +0100

    piano has keys

--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -1,6 +1,9 @@
 import instruments.InstrumentType;
 import instruments.Piano;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PianoTest {
 
@@ -10,6 +13,13 @@ public class PianoTest {
     public void before() {
         piano = new Piano("wood", "brown", InstrumentType.KEYBOARD, "Yamaha", 900.00, 1200.00, 88);
     }
+
+    @Test
+    public void hasStrings(){
+        assertEquals(88, piano.getNumberOfStrings());
+    }
+
+
 
 
 }
