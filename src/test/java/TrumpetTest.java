0699ab96bb55a7cff29c54e336db7791a1dcc1f8
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-11 12:39:43 +0100

    trumpet has valves

--- src/test/java/TrumpetTest.java
+++ src/test/java/TrumpetTest.java
@@ -1,6 +1,9 @@
 import instruments.InstrumentType;
 import instruments.Trumpet;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TrumpetTest {
 
@@ -9,5 +12,10 @@ public class TrumpetTest {
     @Before
     public void before() {
         trumpet = new Trumpet("brass", "gold", InstrumentType.BRASS, "Yamaha", 250.00, 300.00, 3);
+    }
+
+    @Test
+    public void hasValves(){
+        assertEquals(3, trumpet.getNumberOfValves());
     }
 }
