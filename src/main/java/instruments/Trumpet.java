becf2b77e488191e42641e0b443892b544767725
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-11 12:35:43 +0100

    trumpet set up

--- src/main/java/instruments/Trumpet.java
+++ src/main/java/instruments/Trumpet.java
@@ -2,7 +2,14 @@ package instruments;
 
 public class Trumpet extends Instrument {
 
-    public Trumpet(String material, String colour, InstrumentType instrumentType, String make, double buyPrice, double sellPrice) {
+    private int numberOfValves;
+
+    public Trumpet(String material, String colour, InstrumentType instrumentType, String make, double buyPrice, double sellPrice, int numberOfValves) {
         super(material, colour, instrumentType, make, buyPrice, sellPrice);
+        this.numberOfValves = numberOfValves;
+    }
+
+    public int getNumberOfValves() {
+        return numberOfValves;
     }
 }
--- src/test/java/TrumpetTest.java
+++ src/test/java/TrumpetTest.java
@@ -1,2 +1,13 @@
+import instruments.InstrumentType;
+import instruments.Trumpet;
+import org.junit.Before;
+
 public class TrumpetTest {
+
+    Trumpet trumpet;
+
+    @Before
+    public void before() {
+        trumpet = new Trumpet("brass", "gold", InstrumentType.BRASS, "Yamaha", 250.00, 300.00, 3);
+    }
 }
